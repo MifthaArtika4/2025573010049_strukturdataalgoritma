e1f21fa1d9057078d6fff4aa08363bba7e2dca3d
MifthaArtika4 <mifthaartikayahdini@gmail.com>
2026-04-03 10:39:15 +0700

    Praktikum 2: Dasar JavaScript - variabel, tipe data, operator, kondisional, perulangan

---
+++ praktikum-2/01-variabel.js
@@ -0,0 +1,24 @@
+// 01-variabel.js
+//===============================
+//VARIABEL DAN DEKLARASI
+//===============================
+//---Deklarasi dengan let (nilai bisa diubah) 
+let nama ='Budi';
+let umur ='20';
+let kota ='Banda Aceh';
+
+console.log('===Data Mahasiswa===');
+console.log('Nama  :', nama);
+console.log('Umur  :', umur);
+console.log('Kota  :', kota);
+
+//---mengubah nilai let---
+umur = 21;
+console.log('ulang tahun! Umur sekarang:', umur);
+
+//---Deklarasi dengan const (nilai tidak bisa diubah)
+const jurusan = 'Teknik Informatika';
+const tahunMasuk =2025;
+
+console.log('Jurusan  :', jurusan);
+console.log('Tahun Masuk  :', tahunMasuk);
